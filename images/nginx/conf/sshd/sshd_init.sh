18962dae46157b2e662e4e17adf3fe49f6c13741
sylus <sylus1984@gmail.com>
2022-05-10 12:28:16 -0400

    feat(ssh): Add ability to read envvars

--- images/nginx/conf/sshd/sshd_init.sh
+++ images/nginx/conf/sshd/sshd_init.sh
@@ -3,5 +3,9 @@
 echo "Starting SSH..."
 /usr/sbin/sshd
 
+# Get environment variables to show up in SSH session
+# https://docs.microsoft.com/en-us/answers/questions/179503/webapp-for-containers-app-settings-not-passed-thro.html
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
 echo "Starting Nginx..."
 exec /docker-entrypoint.sh "$@"
